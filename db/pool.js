5f88dd5c6e7af8a811f1e39b72f46a8307ebe1cf
OUARRICH <warichsaid@gmail.com>
2020-01-08 00:04:35 +0000

    fix: variabilize database url

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const connectionString = 'postgres://vxndtprgcxjcay:38722fd8f4de87a88c4add16f3eae216cecae23504182fedb6fd114938993dca@ec2-54-217-206-65.eu-west-1.compute.amazonaws.com:5432/d38rcgoqmq43v7';
 
 const pool = new Pool({
-    connectionString: connectionString,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
 });
 
