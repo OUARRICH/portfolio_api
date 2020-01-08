a243270012e06d79453f94a8fc6798c2b8ae767a
OUARRICH <warichsaid@gmail.com>
2020-01-08 18:56:27 +0000

    fix: local database connection

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
-// const connectionString = 'postgresql://me:password@localhost:5432/portfolio';
-const connectionString = 'postgres://vxndtprgcxjcay:38722fd8f4de87a88c4add16f3eae216cecae23504182fedb6fd114938993dca@ec2-54-217-206-65.eu-west-1.compute.amazonaws.com:5432/d38rcgoqmq43v7';
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL || 'postgresql://me:password@localhost:5432/portfolio',
+    ssl: true /**remove ssl in local */
 });
 
 module.exports = pool;
